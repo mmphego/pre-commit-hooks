ae06afabf49f55c3a411e93010407f532adf47a5
Mpho Mphego <mpho112@gmail.com>
2019-10-01 08:18:46 +0200

    Added additional dependency scripts, updated black to support PY2 and PY3 formats

---
+++ scripts/shellcheck.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+################################################################################
+# Ensure shell scripts conform to shellcheck.
+################################################################################
+
+set -eu
+
+readonly DEBUG=${DEBUG:-unset}
+if [ "${DEBUG}" != unset ]; then
+    set -x
+fi
+
+if ! command -v shellcheck >/dev/null  2>&1; then
+    echo 'shellcheck not installed; Please install shellcheck:'
+    echo '  sudo apt-get install -y shellcheck'
+    exit 1
+fi
+
+shellcheck "$@"
+
